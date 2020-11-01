0d092c589aa382b7aee236d57a871185e377b38d
Mahmud-hasan247 <mahmudhasan5406@gmail.com>
2020-11-01 18:28:22 +0600

    some changes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -21,8 +21,31 @@ const Projects = () => {
                                     <div className="img-text">
                                         <h1>Creative Agency</h1>
                                         <h2>A full stack and responsive service seller web application</h2>
-                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, incidunt illum placeat ut vitae corporis!</p>
-                                        <i className="fa fa-github"></i>
+                                        <div className="d-flex">
+                                            
+                                        </div>
+                                        <div className="row justify-content-around">
+                                        <p className='skills'>JavaScript</p>
+                                            <p className='skills'>ReactJS</p>
+                                            <p className='skills'>ES6</p>
+                                            <p className='skills'>React Router</p>
+                                            <p className='skills'>HTML5</p>
+                                            <p className='skills'>CSS3</p>
+                                            <p className='skills'>Bootstrap</p>
+                                            <p className='skills'>Node.js</p>
+                                            <p className='skills'>Express</p>
+                                            <p className='skills'>MongoDB</p>
+                                            <p className='skills'>Firebase</p>
+                                            <p className='skills'>Heroku</p>
+                                        </div>
+
+
+
+
+                                        <div className="row justify-content-around mt-1">
+                                            <button className='btn mr-3 btn-success'><a href="https://creative-agency-391a7.web.app/" target="_blank">Visit website</a></button>
+                                            <button className='btn ml-5  btn-primary'><a href="https://github.com/Mahmud-hasan247/travel-guru" target="_blank">View code</a></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -31,43 +54,84 @@ const Projects = () => {
                             <div className="box">
                                 <div className="profile-box">
                                     <div className="img-area">
-                                        <img  className='project-img' src={image2} alt="" />
+                                        <img className='project-img' src={image2} alt="" />
                                     </div>
                                     <div className="img-text">
                                         <h1>Volunteer Network</h1>
                                         <h2>A full stack social event management web application</h2>
-                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, incidunt illum placeat ut vitae corporis!</p>
-                                        <i className="fa fa-github"></i>
+                                        <div className="d-flex">
+                                           
+                                        </div>
+                                        <div className="row justify-content-around">
+                                        <p className='skills'>JavaScript</p>
+                                            <p className='skills'>ReactJS</p>
+                                            <p className='skills'>ES6</p>
+                                            <p className='skills'>React Router</p>
+                                            <p className='skills'>HTML5</p>
+                                            <p className='skills'>CSS3</p>
+                                            <p className='skills'>Bootstrap</p>
+                                            <p className='skills'>Node.js</p>
+                                            <p className='skills'>Express</p>
+                                            <p className='skills'>MongoDB</p>
+                                            <p className='skills'>Firebase</p>
+                                            <p className='skills'>Heroku</p>
+                                        </div>
+                                        <div className="row justify-content-around mt-3">
+                                            <button className='btn btn-success mr-3'><a href="https://volunteer-network-a05f8.web.app/" target="_blank">Visit website</a></button>
+                                            <button className='btn btn-primary ml-5'><a href="https://github.com/Mahmud-hasan247/volunteer-network" target="_blank">View code</a></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div style={{ marginTop: '320px' , marginBottom: '20px'}} className="col-md-6">
+                        <div style={{ marginTop: '320px', marginBottom: '20px' }} className="col-md-6">
                             <div className="box">
                                 <div className="profile-box">
                                     <div className="img-area">
-                                        <img  className='project-img' src={image3} alt="" />
+                                        <img className='project-img' src={image3} alt="" />
                                     </div>
                                     <div className="img-text">
                                         <h1>Travel Guru</h1>
                                         <h2>A travel management web application</h2>
-                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, incidunt illum placeat ut vitae corporis!</p>
-                                        <i className="fa fa-github"></i>
+                                        <div className="d-flex">
+
+                                        </div>
+                                        <div className="row justify-content-around">
+                                            <p className='skills'>JavaScript</p>
+                                            <p className='skills'>ReactJS</p>
+                                            <p className='skills'>ES6</p>
+                                            <p className='skills'>React Router</p>
+                                            <p className='skills'>HTML5</p>
+                                            <p className='skills'>CSS3</p>
+                                            <p className='skills'>Material UI</p>
+                                            <p className='skills'>Netlify</p>
+                                        </div>
+                                        <div className="row justify-content-around mt-5">
+                                            <button className='btn btn-success'><a className="" href="https://angry-allen-78e7c5.netlify.app" target="_blank">Visit website</a></button>
+                                            <button className="btn btn-primary"><a href="https://github.com/Mahmud-hasan247/travel-guru" target="_blank">View code</a></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div style={{ marginTop: '320px', marginBottom: '20px'}} className="col-md-6">
+                        <div style={{ marginTop: '320px', marginBottom: '20px' }} className="col-md-6">
                             <div className="box">
                                 <div className="profile-box">
                                     <div className="img-area">
-                                        <img className='project-img'  src={image4} alt="" />
+                                        <img className='project-img' src={image4} alt="" />
                                     </div>
                                     <div className="img-text">
-                                        <h1>Travel Guru</h1>
-                                        <h2>A travel management web application</h2>
-                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, incidunt illum placeat ut vitae corporis!</p>
-                                        <i className="fa fa-youtube"></i>
+                                        <h1>E-school</h1>
+                                        <h2>A responsive landing page for an E-school</h2>
+                                        <div className="row mt-5 justify-content-center">
+                                            <p className='skills'>HTML5</p>
+                                            <p className='skills'>CSS3</p>
+                                            <p className='skills'>Bootstrap</p>
+                                        </div>
+                                        <div className="row justify-content-around mt-5 pt-5">
+                                            <button className='btn btn-success'><a className="" href="https://mahmud-hasan247.github.io/E-school/" target="_blank">Visit website</a></button>
+                                            <button className="btn btn-primary"><a href="https://github.com/Mahmud-hasan247/E-school" target="_blank">View code</a></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
